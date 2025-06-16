26f898506d6fd2cfd4d6a764f09eed59c1c62dac
sebas0-0 <sebastianbarrbonilla.123@gmail.com>
2025-06-15 21:16:57 -0600

    correccion de errores

---
+++ app/src/main/java/com/example/listsee/viewModelList/ListViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.listsee.viewModelList
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.listsee.core.ResultWrapper
+import com.example.listsee.model.Task
+import com.example.listsee.network.TaskRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ListViewModel  @Inject constructor(
+
+private val repository: TaskRepository
+
+): ViewModel() {
+    private val _loaderState = MutableLiveData<Boolean>()
+    val loaderState: LiveData<Boolean>
+    get() = _loaderState
+
+    private val _taskInfo = MutableLiveData<List<Task>>()
+    val taskInfo: LiveData<List<Task>>
+    get() = _taskInfo
+
+    fun fetchAllTasks() {
+        _loaderState.value = true
+
+        viewModelScope.launch {
+            when (val result = repository.getAllTasks()) {
+                is ResultWrapper.Success -> {
+                    _loaderState.value = false
+                    _taskInfo.value = result.data
+                }
+                is ResultWrapper.Error -> {
+                    _loaderState.value = false
+                    val errorMessage = result.exception.message
+                }
+            }
+        }
+    }
+}
